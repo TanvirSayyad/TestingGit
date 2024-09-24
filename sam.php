b03944bdf5bf9fc021896cde1f8485c77188ae6c
Tanvir <Sayyadtanvir88@gmail.com>
2024-09-24 14:21:15 +0530

    v6.1 .

--- sam.php
+++ sam.php
@@ -7,15 +7,8 @@
     <link rel="stylesheet" href="Style.css">
 </head>
 <body>
-    <form action="">
-<input type="text" >
-<button>Submit</button>
-<div>
 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem tenetur voluptatum
- odio amet consequatur. Voluptatibus veniam eos, tempora rem earum, deserunt tenetur excepturi sapiente 
- quam recusandae molestias itaque sint modi.
-</div>
+<h1> this is v7</h1>
 
 
     </form>
